3326d42a848001ddf290a3cec3cb092f638cbbbb
Hanbey Ozan Kırmızıgül <118853484+Melfo03@users.noreply.github.com>
2024-12-18 04:46:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamirHane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool cikis = false;
+            while (cikis != true)
+            {
+                uint aracSayi = 0;
+                uint tekneSayi = 0;
+                Console.WriteLine("****************************Hosgeldiniz****************************");
+                
+                Console.WriteLine("Eklemek İstediğiniz Arac Tipini Seciniz");
+                Console.WriteLine("1-Araba\n2-Tekne");
+                int tipSecim = Convert.ToInt32(Console.ReadLine());
+                switch (tipSecim)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("Araç Sahibinin İsimini Giriniz: "); string aa = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Aracın Markasını Giriniz: "); string ab = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Aracın Model Yilini Giriniz: "); int ac = Convert.ToInt32(Console.ReadLine()); Console.Clear();
+                        Console.WriteLine("Degisim Yapilacak Parca Sayisini Giriniz: "); int ad = Convert.ToInt32(Console.ReadLine()); Console.Clear();
+                        Console.WriteLine("Araca Yapilacak Islemi Giriniz: "); string ae = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Aracın Rengini Giriniz: "); string af = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Aracın Motor Tipini Giriniz: "); string ag = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Aracın Kilometresini Giriniz: "); uint ah = Convert.ToUInt32(Console.ReadLine()); Console.Clear();
+
+                        Araba araba1 = new Araba(aa, ab, ac, ad, ae, af, ag, ah, aracSayi);
+                        aracSayi++;
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Tekne Sahibinin İsimini Giriniz: "); string ta = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Teknenin Markasını Giriniz: "); string tb = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Teknenin Model Yilini Giriniz: "); int tc = Convert.ToInt32(Console.ReadLine()); Console.Clear();
+                        Console.WriteLine("Degisim Yapilacak Parca Sayisini Giriniz: "); int td = Convert.ToInt32(Console.ReadLine()); Console.Clear();
+                        Console.WriteLine("Tekneye Yapilacak Islemi Giriniz: "); string te = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Teknenin Rengini Giriniz: "); string tf = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Teknenin Gövde Tipini Giriniz: "); string tg = Console.ReadLine(); Console.Clear();
+                        Console.WriteLine("Teknenin Çalişma Saatini Giriniz: "); uint th = Convert.ToUInt32(Console.ReadLine()); Console.Clear();
+
+
+                        Tekne tekne1 = new Tekne(ta, tb, tc, td, te, tf, tg, th, tekneSayi);
+                        tekneSayi++;
+                        break;
+                }
+                
+                Console.Write("\n\nYeni Vasıta Ekle (E/H)");
+                char cikmak = Convert.ToChar(Console.ReadLine());
+                Console.Clear();
+                if (cikmak == 'h' || cikmak == 'H')
+                {
+                    cikis = true; return;
+                }
+                else
+                {
+                    cikis = false;
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Vasita_class.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TamirHane
+{
+    public class Vasita
+    {
+        public string vasitaSahibi;
+        public string marka;
+        public int model_yili;
+        public int parcaSayisi;
+        public string yapilanİslem;
+        
+          public Vasita(string _vasitaSahibi, string _marka, int _model_yili, int _parcaSayisi, string _yapilanİslem) 
+        {
+            this.vasitaSahibi = _vasitaSahibi;
+            this.marka = _marka;
+            this.model_yili = _model_yili;
+            this.parcaSayisi = _parcaSayisi;
+            this.yapilanİslem = _yapilanİslem;
+            
+        
+        }
+    }
+    public class Araba :Vasita
+    {
+        public string arabaRengi;
+        public string aracMotorTipi;
+        public uint aracKm;
+        public uint aracSayisi;
+          public Araba(string vasitaSahibi, string marka,int model_yili, int parcaSayisi,string yapilanİslem,string _arabaRengi, string _aracMotorTipi, uint _aracKm,uint _aracSayisi):base(vasitaSahibi,marka,model_yili,parcaSayisi,yapilanİslem)
+        {
+            this.arabaRengi = _arabaRengi;
+            this.aracMotorTipi = _aracMotorTipi;
+            this.aracKm = _aracKm;
+            this.aracSayisi= _aracSayisi;
+            _aracSayisi++;
+            DateTime girisTarihi = DateTime.Now;
+            Console.WriteLine("Araç Sahibinin Adı:{0}\nAracın Markası:{1}\nAracın Model Yili:{2}\nAracın Rengi:{3}\nAracın Motor Tipi:{4}\nAracın Kilometresi:{5}\nAraca Yapilacak İslem:{6}\nİslemde Kullanilacak Parca Sayisi:{7}\nServise Giriş Tarihi:{8}\nServisteki Araç Sayisi:{9}", vasitaSahibi,marka,model_yili,_arabaRengi,_aracMotorTipi,_aracKm,yapilanİslem,parcaSayisi,girisTarihi,_aracSayisi);
+
+        }
+    }
+    public class Tekne : Vasita 
+    {
+        public string tekneRengi;
+        public string govde;
+        public uint tekneCalismaSaati; 
+        public uint tekneSayisi;
+        
+        public Tekne(string vasitaSahibi, string marka, int model_yili, int parcaSayisi, string yapilanİslem, string _tekneRengi, string _govde, uint _tekneCalismaSaati, uint _tekneSayisi) : base(vasitaSahibi, marka, model_yili, parcaSayisi, yapilanİslem)
+        {
+            this.tekneRengi = _tekneRengi;
+            this.govde = _govde;
+            this.tekneCalismaSaati = _tekneCalismaSaati;
+            this.tekneSayisi = _tekneSayisi;
+            _tekneSayisi++;
+            DateTime girisTarihi = DateTime.Now;
+            Console.WriteLine("Tekne Sahibinin Adı:{0}\nTeknenin Markası:{1}\nTeknenin Model Yili:{2}\nTeknenin Rengi:{3}\nTeknenin Gövde Tipi:{4}\nTeknenin Çalışma Saati:{5}\nTekneye Yapilacak İslem:{6}\nİslemde Kullanilacak Parca Sayisi:{7}\nServise Giriş Tarihi:{8}\nServisteki Tekne Sayisi:{9}", vasitaSahibi, marka, model_yili, _tekneRengi, _govde,_tekneCalismaSaati, yapilanİslem, parcaSayisi, girisTarihi, _tekneSayisi);
+        }
+    }
+}
